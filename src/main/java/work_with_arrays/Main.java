da6186e393a2bf71a2bf4cdd846b91616b1c263e
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-16 12:03:11 +0300

    Add example of working wit arrays

---
+++ src/main/java/work_with_arrays/Main.java
@@ -0,0 +1,24 @@
+package work_with_arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] elements = new int[8];
+        elements[0] = 1;
+        elements[1] = 4;
+
+        int[] anArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
+        String[][] names = {
+                {"Mr. ", "Mrs. ", "Ms. "},
+                {"Smith", "Jones"}
+        };
+
+        int[][] jagged = new int[3][];
+        jagged[0] = new int[]{1, 2};
+        jagged[1] = new int[]{3, 4, 5, 6, 7, 8};
+        jagged[2] = new int[]{9, 10, 11};
+
+    }
+
+
+}
