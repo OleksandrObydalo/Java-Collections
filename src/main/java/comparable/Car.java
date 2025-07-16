0ccf9a1248b0b5de697b3033e6edbe55f712b0df
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-16 12:16:43 +0300

    Add example with Comparable interface

---
+++ src/main/java/comparable/Car.java
@@ -0,0 +1,12 @@
+package comparable;
+
+class Car implements Comparable<Car> {
+    private String model;
+    public Car(String model) { this.model = model; }
+    public String getModel() { return model; }
+    @Override
+    public int compareTo(Car other) {
+        return this.model.compareTo(other.model);
+    }
+}
+
